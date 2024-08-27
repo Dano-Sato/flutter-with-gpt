c85f7dcf6b03c0e6476c6f528c61a231976c1816
Dano Sato <ten0102@naver.com>
2024-08-27 23:14:59 +0900

    snackBar

--- lib/4_mini.dart
+++ lib/4_mini.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/// 앱 바와 탭 바가 있는 홈 화면.
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -24,7 +25,15 @@ class HomeScreen extends StatelessWidget {
       length: 3, // 탭의 개수
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Snackbar'),
+          title: Row(
+            children: [
+              SizedBox(width: 20),
+              Icon(Icons.fastfood, color: Colors.amber),
+              SizedBox(width: 10),
+              Text("Snackbar"),
+            ],
+          ),
+          centerTitle: true,
           bottom: TabBar(
             tabs: [
               Tab(icon: Icon(Icons.home), text: 'Home'),
@@ -58,33 +67,29 @@ class MyWidget extends StatelessWidget {
           ElevatedButton(
             onPressed: () {
                   final scaffoldMessenger = ScaffoldMessenger.of(context);
-          
+                  scaffoldMessenger.hideCurrentSnackBar();
                   // SnackBar를 표시합니다.
                   final snackBar = SnackBar(
                     content: Text('Hello from a SnackBar!'),
                     duration: Duration(seconds: 3), // 스낵바의 표시 시간
                   );
           
-                  scaffoldMessenger.showSnackBar(snackBar);
-          
-                  // 2초 후에 스낵바를 자동으로 숨깁니다.
-                  Future.delayed(Duration(seconds: 2), () {
-                    scaffoldMessenger.hideCurrentSnackBar();
-                  });            
+                  scaffoldMessenger.showSnackBar(snackBar);          
                 },
             child: Text('Say hello to a SnackBar'),
           ),
           SizedBox(height: 20),
-          ElevatedButton(onPressed: () {
+          ElevatedButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
               ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(
                 content: Text('F**k you too from a SnackBar!'),
-                duration: Duration(seconds: 2), // 스낵바의 표시 시간
+                duration: Duration(seconds: 3), // 스낵바의 표시 시간
               ),
               );
-
-          }, 
-          child: Text("Say f**k you to a SnackBar!"))
+            }, 
+            child: Text("Say f**k you to a SnackBar!"))
         ],
       ),
     );
@@ -104,6 +109,7 @@ class MyListView extends StatelessWidget {
           leading: Icon(Icons.star),
           title: Text('Item $index'),
           onTap: () { // 아이템을 클릭했을 때의, 스낵바에서 어떤 아이템을 클릭했는지 알려준다.
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
             ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(
                 content: Text('You clicked Item $index!'),
